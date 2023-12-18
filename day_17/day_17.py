21c112e711970d2e5601a583dd72214cf3169620
ster <ster@pm.me>
2023-12-18 14:52:03 +0100

    Day 17 part 2

--- day_17/day_17.py
+++ day_17/day_17.py
@@ -1,20 +1,23 @@
 import heapq
 from collections import defaultdict
-from copy import copy
 from dataclasses import dataclass
 from enum import Enum
 
 
 def part_one(input_file: str):
-    graph = get_graph(input_file)
-    start_node = Node(0, 0, 3, Direction.EAST)
-    distances, came_from, end = dijkstra(graph, start_node.copy())
-
+    min_steps, max_steps = 1, 3
+    graph = get_graph(input_file, min_steps, max_steps)
+    start_node = Node(0, 0, max_steps, Direction.EAST)
+    distances, end = dijkstra(graph, start_node.copy())
     return distances[end] - graph.crucibles[-1][-1] + 1
 
 
 def part_two(input_file: str):
-    raise NotImplementedError
+    min_steps, max_steps = 4, 10
+    graph = get_graph(input_file, min_steps, max_steps)
+    start_node = Node(0, 0, max_steps, Direction.EAST)
+    distances, end = dijkstra(graph, start_node.copy())
+    return distances[end]
 
 
 class Direction(Enum):
@@ -31,10 +34,8 @@ class Node:
     remaining_steps: int
     direction: Direction
 
-    # turned: bool = False
-
     def copy(self):
-        return copy(self)
+        return Node(self.y, self.x, self.remaining_steps, self.direction)
 
     def move(self):
         y_delta, x_delta = self.direction.value
@@ -42,57 +43,66 @@ class Node:
         self.x += x_delta
         self.remaining_steps -= 1
 
-    def turn_left(self):
+    def turn_left(self, max_steps: int):
         self.direction = {
             Direction.NORTH: Direction.WEST,
             Direction.EAST: Direction.NORTH,
             Direction.SOUTH: Direction.EAST,
             Direction.WEST: Direction.SOUTH,
         }[self.direction]
-        self.remaining_steps = 3
+        self.remaining_steps = max_steps
 
-    def turn_right(self):
+    def turn_right(self, max_steps: int):
         self.direction = {
             Direction.NORTH: Direction.EAST,
             Direction.EAST: Direction.SOUTH,
             Direction.SOUTH: Direction.WEST,
             Direction.WEST: Direction.NORTH,
         }[self.direction]
-        self.remaining_steps = 3
+        self.remaining_steps = max_steps
 
     def __hash__(self):
         return hash((self.y, self.x, self.remaining_steps, self.direction))
 
     def __lt__(self, other):
-        if self.remaining_steps == other.remaining_steps:
-            return (self.y, self.x) < (other.y, other.x)
-        return self.remaining_steps < other.remaining_steps
+        return (self.remaining_steps, self.y, self.x) < (other.remaining_steps, other.y, other.x)
 
 
 class Graph:
-    def __init__(self, crucibles: list[list[int]]):
+    def __init__(self, crucibles: list[list[int]], min_steps: int, max_steps: int):
         self.crucibles: list[list[int]] = crucibles
         self.width = len(crucibles[0])
         self.height = len(crucibles)
-        # self.size = self.width * self.height
+        self.min_steps = min_steps
+        self.max_steps = max_steps
 
     def get_neighbors(self, node: Node):
         yield from self.advance(node)
 
         left_node = node.copy()
-        left_node.turn_left()
+        left_node.turn_left(self.max_steps)
         yield from self.advance(left_node)
 
         right_node = node.copy()
-        right_node.turn_right()
+        right_node.turn_right(self.max_steps)
         yield from self.advance(right_node)
 
     def advance(self, node: Node):
-        if node.remaining_steps <= 0:
-            return
-
-        tmp_node = node.copy()
+        tmp_node = node
         weight = 0
+
+        # If the node just rotated,
+        # we need to advance the minimum numbers of steps first
+        if tmp_node.remaining_steps == self.max_steps:
+            tmp_node = tmp_node.copy()
+            for _ in range(self.min_steps):
+                tmp_node.move()
+                if self.out_of_map(tmp_node):
+                    return
+                weight += self.crucibles[tmp_node.y][tmp_node.x]
+            yield tmp_node, weight
+
+        # We then return each remaining step individually
         while tmp_node.remaining_steps > 0:
             tmp_node = tmp_node.copy()
             tmp_node.move()
@@ -107,7 +117,6 @@ class Graph:
 
 def dijkstra(graph: Graph, start: Node):
     distances = defaultdict(lambda: float("inf"))
-    predecessors = {}
     distances[start] = 0
     priority_queue = [(0, start)]
 
@@ -122,27 +131,15 @@ def dijkstra(graph: Graph, start: Node):
 
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
-                predecessors[neighbor] = current_vertex
                 heapq.heappush(priority_queue, (distance, neighbor))
 
             if (neighbor.x == graph.width - 1) and (neighbor.y == graph.height - 1):
-                return distances, predecessors, neighbor
+                return distances, neighbor
 
     raise ValueError("No solution found for the given graph")
 
 
-def get_path(predecessors, start, end):
-    path = []
-    current = end
-    while current != start:
-        path.append(current)
-        current = predecessors[current]
-    path.append(start)
-    path.reverse()
-    return path
-
-
-def get_graph(input_file: str) -> Graph:
+def get_graph(input_file: str, min_steps: int, max_steps: int) -> Graph:
     crucibles = []
     with open(input_file) as f:
         for y, line in enumerate(f.readlines()):
@@ -150,4 +147,4 @@ def get_graph(input_file: str) -> Graph:
             for x, value in enumerate(line.strip()):
                 crucible_row.append(int(value))
             crucibles.append(crucible_row)
-    return Graph(crucibles)
+    return Graph(crucibles, min_steps, max_steps)
